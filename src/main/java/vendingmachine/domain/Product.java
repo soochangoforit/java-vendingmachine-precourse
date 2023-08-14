d20620cd09369fbbdf6e1cfb005fcf0f176de0b8
soochan <tncksdl05@gmail.com>
2023-08-14 19:45:35 +0900

    Feat(Product): 상품을 의미하는 객체 추가

---
+++ src/main/java/vendingmachine/domain/Product.java
@@ -0,0 +1,15 @@
+package vendingmachine.domain;
+
+public class Product {
+
+    private final String name;
+    private final int price;
+    private final int count;
+
+    private Product(String name, int price, int count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+}
