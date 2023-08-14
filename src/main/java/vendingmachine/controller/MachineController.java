c928c2711f0dbb838f45761fff845bc549505b3c
soochan <tncksdl05@gmail.com>
2023-08-14 19:23:47 +0900

    Feat(Controller): 재입력 기능 적용

--- src/main/java/vendingmachine/controller/MachineController.java
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -1,5 +1,8 @@
 package vendingmachine.controller;
 
+import static vendingmachine.util.RetryUtil.read;
+
+import vendingmachine.dto.MachineMoneyDto;
 import vendingmachine.view.InputView;
 
 public class MachineController {
@@ -7,6 +10,9 @@ public class MachineController {
     private final InputView inputView = InputView.init();
 
     public void run() {
+        // 자판기가 보유하고 있는 금액을 입력해 주세요.자판기가 보유하고 있는 금액을 입력해 주세요.
+        MachineMoneyDto machineMoneyDto = read(inputView::readMachineMoney);
+
 
     }
 }
