769734d935748325cc07ec8e74364583b1ce3d03
soochan <tncksdl05@gmail.com>
2023-08-14 18:42:32 +0900

    Feat(Controller): 기본 구조 설정

---
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -0,0 +1,12 @@
+package vendingmachine.controller;
+
+import vendingmachine.view.InputView;
+
+public class MachineController {
+
+    private final InputView inputView = InputView.init();
+
+    public void run() {
+
+    }
+}
---
+++ src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,11 @@
+package vendingmachine.view;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static InputView init() {
+        return new InputView();
+    }
+}
