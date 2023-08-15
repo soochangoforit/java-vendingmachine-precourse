dea08852630d486b388a79bb59aeb97f78e618b4
soochan <tncksdl05@gmail.com>
2023-08-15 14:12:04 +0900

    Refactor(InputView): 출력 메시지 상수화

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -11,6 +11,10 @@ import vendingmachine.util.InputValidator;
 
 public class InputView {
 
+    private final String INPUT_MACHINE_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private final String INPUT_PRODUCTS = "상품명과 가격, 수량을 입력해 주세요.";
+    private final String INPUT_USER_MONEY = "투입 금액을 입력해 주세요.";
+
     private InputView() {
     }
 
@@ -19,24 +23,30 @@ public class InputView {
     }
 
     public MachineMoneyDto readMachineMoney() {
-        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        System.out.println(INPUT_MACHINE_MONEY);
         String rawMachineMoney = Console.readLine();
+        printEmptyLine();
         InputValidator.validateMachineMoney(rawMachineMoney);
         int machineMoney = InputConverter.convertMachineMoney(rawMachineMoney);
-        System.out.println();
         return new MachineMoneyDto(machineMoney);
     }
 
+    private static void printEmptyLine() {
+        System.out.println();
+    }
+
     public List<ProductDto> readProducts() {
-        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+        System.out.println(INPUT_PRODUCTS);
         String inputProducts = Console.readLine();
+        printEmptyLine();
         InputValidator.validateProducts(inputProducts);
         return InputConverter.convertProducts(inputProducts);
     }
 
     public UserMoneyDto readUserMoney() {
-        System.out.println("투입 금액을 입력해 주세요.");
+        System.out.println(INPUT_USER_MONEY);
         String rawUserMoney = Console.readLine();
+        printEmptyLine();
         InputValidator.validateUserMoney(rawUserMoney);
         int userMoney = InputConverter.convertUserMoney(rawUserMoney);
         return new UserMoneyDto(userMoney);
