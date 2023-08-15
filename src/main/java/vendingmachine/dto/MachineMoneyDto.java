9f8ea483b5c7bb1177dccebb0404ce58a2001e75
soochan <tncksdl05@gmail.com>
2023-08-15 13:47:42 +0900

    Refactor(MachineMoneyDto): 도메인 객체에서 유효성 검사를 하도록 변경

--- src/main/java/vendingmachine/dto/MachineMoneyDto.java
+++ src/main/java/vendingmachine/dto/MachineMoneyDto.java
@@ -1,29 +1,12 @@
 package vendingmachine.dto;
 
-import static vendingmachine.domain.ExceptionMessage.INVALID_MACHINE_MONEY;
-import static vendingmachine.domain.MachineMoney.DIVIDABLE_MIN_MONEY;
-import static vendingmachine.domain.MachineMoney.ZERO;
-
 public class MachineMoneyDto {
-
 
     private final int machineMoney;
 
     public MachineMoneyDto(int machineMoney) {
-        validate(machineMoney);
         this.machineMoney = machineMoney;
     }
-
-    private void validate(int machineMoney) {
-        if (!isDividable(machineMoney)) {
-            throw new IllegalArgumentException(INVALID_MACHINE_MONEY);
-        }
-    }
-
-    private boolean isDividable(int machineMoney) {
-        return machineMoney % DIVIDABLE_MIN_MONEY == ZERO;
-    }
-
 
     public int getMachineMoney() {
         return machineMoney;
