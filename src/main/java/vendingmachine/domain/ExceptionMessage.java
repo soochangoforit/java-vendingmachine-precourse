62b351256b0932f347081d22b1e61015045d9c32
soochan <tncksdl05@gmail.com>
2023-08-15 17:16:46 +0900

    Feat(ExceptionMessage): 예외 상황에 대한 메시지 상수화

--- src/main/java/vendingmachine/domain/ExceptionMessage.java
+++ src/main/java/vendingmachine/domain/ExceptionMessage.java
@@ -6,8 +6,10 @@ public class ExceptionMessage {
     public static final String INVALID_COIN_MONEY = "코인을 생성할 적합한 금액이 없습니다.";
     public static final String INVALID_PRODUCT_PRICE = "상품의 가격은 100원 이상여야 합니다.";
     public static final String INVALID_USER_MONEY = "투입 금액은 0원 이상이어야 합니다.";
-
     public static final String INVALID_PRODUCT_NAME = "존재하지 않는 상품 이름입니다.";
+    public static final String INVALID_PRODUCT = "상품이 존재하지 않습니다.";
+    public static final String SOLD_OUT_PRODUCT = "품절된 상품입니다.";
+    public static final String NOT_ENOUGH_MONEY = "해당 상품을 구매하기 위한 잔돈이 부족합니다.";
 
     private ExceptionMessage() {
     }
