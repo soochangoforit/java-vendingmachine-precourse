cc2e6cf7f47da3d0a40cef6d481b2cd5397c87c5
soochan <tncksdl05@gmail.com>
2023-08-15 12:44:16 +0900

    Feat(ProductDto): 상품 입력을 매핑하기 위한 dto 추가

---
+++ src/main/java/vendingmachine/dto/ProductDto.java
@@ -0,0 +1,34 @@
+package vendingmachine.dto;
+
+import vendingmachine.util.NullOrEmptyValidator;
+
+public class ProductDto {
+    private final String name;
+    private final int price;
+    private final int quantity;
+
+    public ProductDto(String name, int price, int quantity) {
+        validate(name, price);
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    private void validate(String name, int price) {
+        NullOrEmptyValidator.throwIfBlank(name);
+        if (!isMinimumPrice(price)) {
+            throw new IllegalArgumentException("가격은 100원 이상이어야 합니다.");
+        }
+        if (!isDividable(price)) {
+            throw new IllegalArgumentException("가격은 10원 단위로 입력해주세요.");
+        }
+    }
+
+    private boolean isDividable(int price) {
+        return price % 10 == 0;
+    }
+
+    private static boolean isMinimumPrice(int price) {
+        return price >= 100;
+    }
+}
